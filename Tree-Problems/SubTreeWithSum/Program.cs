e83fa6d08a9d3ee4b63dd31acc5224723ca9b62b
Aman Singh Parihar <amanparihar16@gmail.com>
2024-12-27 16:25:49 +0530

    Sub tree with sum

---
+++ Tree-Problems/SubTreeWithSum/Program.cs
@@ -0,0 +1,21 @@
+﻿// https://www.geeksforgeeks.org/subtree-given-sum-binary-tree/?ref=lbp
+
+namespace SubTreeWithSum;
+class Node
+{
+    public int data;
+    public Node? left;
+    public Node? right;
+    public Node(int data) => this.data = data;
+}
+class Tree
+{
+
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
