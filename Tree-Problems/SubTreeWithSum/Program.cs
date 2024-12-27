b2ee6524afe3c32ad2af5c1b5cadd7bfd6079004
Aman Singh Parihar <amanparihar16@gmail.com>
2024-12-27 22:25:35 +0530

    subtree sum

--- Tree-Problems/SubTreeWithSum/Program.cs
+++ Tree-Problems/SubTreeWithSum/Program.cs
@@ -10,12 +10,38 @@ class Node
 }
 class Tree
 {
-
+    public static int SubTreeSum(Node? root)
+    {
+        if (root == null) return 0;
+        return root.data + SubTreeSum(root.left) + SubTreeSum(root.right);
+    }
+    public static bool SubTreeWithGivenSum(Node? root, int key)
+    {
+        if (root == null) return false;
+        if (SubTreeSum(root.left) == key) return true;
+        if (SubTreeSum(root.right) == key) return true;
+        return SubTreeWithGivenSum(root.left, key) || SubTreeWithGivenSum(root.right, key);
+    }
 }
 class Program
 {
     public static void Main(string[] args)
     {
+        // Representation of the input tree:
+        //         1
+        //        / \
+        //       2   3
+        //      / \
+        //     4   5
+        Node first = new Node(1)
+        {
+            left = new Node(2),
+            right = new Node(3)
+        };
+        first.left.left = new Node(4);
+        first.left.right = new Node(5);
+        var result = Tree.SubTreeWithGivenSum(first, 15) ? "Matches": "Does Not";
+        Console.WriteLine($"Given Tree {result} with the key");
         
     }
 }
